3ca569accc46e82610776e3d8fb1116f5cb305ee
ByteStream93 <116450794+ByteStream93@users.noreply.github.com>
2022-12-19 08:14:38 +0100

    Update Counter.java

--- src/main/java/com/example/snakegame/Counter.java
+++ src/main/java/com/example/snakegame/Counter.java
@@ -5,8 +5,11 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class Counter {
+    
     public static int points = 0;
+    
     public static void count(GraphicsContext graphicsContext){
+        
         graphicsContext.setFont(Font.font(20));
         graphicsContext.setStroke(Color.WHITE);
         graphicsContext.strokeText("Count: "+ points,17.6*Table.getFieldsize(),0.65*Table.getFieldsize());
