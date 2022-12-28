b60cb75bf308c0ef3c93ebd18269277dc893a241
ByteStream93 <116450794+ByteStream93@users.noreply.github.com>
2022-12-28 19:14:05 +0100

    Update GameOver.java

--- src/main/java/com/example/snakegame/GameOver.java
+++ src/main/java/com/example/snakegame/GameOver.java
@@ -8,16 +8,22 @@ public class GameOver {
 
     public static void end(GraphicsContext graphicsContext){
         int endDrawing = 0;
+        
         if(Main.snake.get(0).x_coordinate > Table.getColumns()-1 || Main.snake.get(0).x_coordinate < 0){
+            
             Refresher.refresh.stop();
             endDrawing = 1;
+            
         }
         if(Main.snake.get(0).y_coordinate > Table.getRows()-1 || Main.snake.get(0).y_coordinate < 0){
             Refresher.refresh.stop();
+            
             endDrawing = 1;
         }
+        
         for (int i = 2; i < Main.snake.size(); i++){
         if(Main.snake.get(i).x_coordinate==Main.snake.get(0).x_coordinate && Main.snake.get(i).y_coordinate==Main.snake.get(0).y_coordinate){
+            
             Refresher.refresh.stop();
             endDrawing = 1;
          }
